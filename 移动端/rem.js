63372a3bba87f34f8c09494021a551fe800bdfb4
fabiyan <169250243@qq.com>
2019-01-17 21:53:41 +0800

    zepto和touch

--- 移动端/rem.js
+++ 移动端/rem.js
@@ -7,7 +7,7 @@
     function refreshRem() {
         // 获取屏幕宽度
         var width = docEl.getBoundingClientRect().width;
-        var rem = width / 10; // 将屏幕宽度分成6.4份， 1份为1rem
+        var rem = width / 6.4; // 将屏幕宽度分成6.4份， 1份为1rem
 
         docEl.style.fontSize = rem + 'px';
         console.log(0.14*rem);
